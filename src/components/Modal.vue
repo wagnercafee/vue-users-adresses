3dcc66cd08b14aec798bdeac40cc4b759bab8f62
wagnercafee <wagnercafee@gmail.com>
2025-12-10 10:17:53 -0300

    Limpa aplicação e inicia estrutura base.

---
+++ src/components/Modal.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="componente">
+        <h1>Modal</h1>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        min-height: 500px;
+    }
+</style>
